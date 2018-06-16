efadd88b362453f9b93121453de4d5349d70dae9
Sergey Korepanov <igzhestik@mail.ru>
2018-06-16 15:48:55 +0500

    Основные классы

---
+++ Airport/Classes/Plane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Classes
+{
+    public class Plane
+    {
+        public PlaneType Model { get; set; }
+        public PlaneDirection Direction { get; set; }
+        public DateTime Time { get; set; }
+        public string City { get; set; }
+        public int NumberOfPassengers { get; set; }
+    }
+}
---
+++ Airport/Classes/PlaneType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Classes
+{
+    public enum PlaneType
+    {
+        Type1 = 100,
+        Type2 = 200,
+        Type3 = 300
+    }
+}
